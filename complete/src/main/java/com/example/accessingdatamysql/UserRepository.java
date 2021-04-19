14c9c8f6c5a27cdab6074032f07ed3ae4f74194b
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-04-19 18:30:58 +0200

    add findByNameIgnoreCase

--- complete/src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ complete/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.accessingdatamysql.User;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+	List<User> findByNameIgnoreCase(String name);
 }
