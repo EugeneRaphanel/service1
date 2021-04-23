69d1c748a9038cd4a36f8e492e89ded2036deac9
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-04-23 12:24:58 +0200

    DTO return by POST PUT and DELETE

---
+++ complete/src/main/java/com/example/accessingdatamysql/DTOanswer.java
@@ -0,0 +1,16 @@
+package com.example.accessingdatamysql;
+
+public class DTOanswer {
+	
+	private boolean sucess;
+	
+
+	public boolean getSucess() {
+		return sucess;
+	}
+
+	public void setSucess(boolean sucess) {
+		this.sucess = sucess;
+	}
+
+}
